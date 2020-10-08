01c856923e471454fcb569b463fc854bcceed23b
Ava Tran <ava.tran@patelhs.org>
2020-10-08 16:40:01 +0000

    I made a scanner in which it asks for the imput of the audience.

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args){
      boolean isSunny = true;
@@ -6,13 +8,28 @@ class Main {
      if (isSunny || temp > 95 && !isRaining )
      {
        System.out.println("Let's go get icecream!");
+       
+        Scanner scan  = new Scanner(System.in);
+
+        System.out.print("How many scoops would you like?");
+        int scoops = scan.nextInt();
+
+        System.out.print(scoops); 
+
+        int count = 0; 
+        while(count < scoops){   
+
+          
+        count ++; 
+        }
+
+
+
      }
       else
      {
        System.out.println("Let's stay inside and watch a movie");
-
        
-  
-     }
+      }
     } 
   }
