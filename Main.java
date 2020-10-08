e39670722ff9f2e1b66bdd0b5348054492a12c6d
Ava Tran <ava.tran@patelhs.org>
2020-10-08 16:07:44 +0000

    So far I have declared and initialized three variables.

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args){
+     boolean isSunny = true;
+     boolean isRaining = false; 
+     int temp = 95;
+     if (isSunny || temp > 95 && !isRaining )
+     {
+       System.out.println("Let's go get icecream!");
+     }
+      else
+     {
+       System.out.println("Let's stay inside and watch a movie");
+
+       
+  
+     }
+    } 
   }
-}
