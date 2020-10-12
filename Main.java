69ec95b8bdab9354d9fe0238c53dfb712984e130
Ava Tran <ava.tran@patelhs.org>
2020-10-12 14:06:15 +0000

    I added another print statement, claiming that it was the perfect day to have icecream.

--- Main.java
+++ Main.java
@@ -2,29 +2,28 @@ import java.util.Scanner;
 
 class Main { 
   
-
-
   public static void main(String[] args){
-     boolean isSunny = true;
-     boolean isRaining = false; 
-     int temp = 95;
-     if (isSunny || temp > 95 && !isRaining )
+      boolean isSunny = true;
+      boolean isRaining = false; 
+      double temp = 95.7;
+     if (isSunny || temp > 95.7 && !isRaining )
      {
        System.out.println("Let's go get icecream!");
 
        Scanner scan  = new Scanner(System.in);
         System.out.print("How many scoops would you like?");
         int scoops = scan.nextInt();
-        System.out.print("Your answer: " + scoops);
+        System.out.print("Your answer: " + scoops); 
 
         int count = 0; 
         while(count < scoops){   
 
-        System.out.println("\n What flavor would you like for every scoop?");string flavor = scan.next;  
-
-        count ++; 
+          System.out.println("\n What flavor would you like for this scoop?");scan.next();  
+      
+        count ++;
+      
         }
-
+          System.out.println("It's a perfect day to have icecream!");
      }
       else
      {
